272a42c9c97a189222387d755938f86003b8cc38
nemanja <nemanja.petrovic@united.cloud>
2021-08-18 22:03:51 +0200

    Fix bug

--- src/main/java/com/met/transaction/document/Transaction.java
+++ src/main/java/com/met/transaction/document/Transaction.java
@@ -24,7 +24,7 @@ public class Transaction {
     @NotNull
     private Integer transactionCode;
     private String identifier;
-    private Double amount;
+    private Double n;
     @JsonSerialize(using = LocalDateTimeSerializer.class)
     @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime transactionTime;
