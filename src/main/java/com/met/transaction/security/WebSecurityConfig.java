f0d00d5426307c87a9563dd9feabb7846edbb072
nemanja <nemanja.petrovic@united.cloud>
2021-08-31 16:41:39 +0200

    Errors

--- src/main/java/com/met/transaction/security/WebSecurityConfig.java
+++ src/main/java/com/met/transaction/security/WebSecurityConfig.java
@@ -46,10 +46,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(
                         HttpMethod.GET,
                         "/",
-                        "/v2/api-docs",           // swagger
-                        "/webjars/**",            // swagger-ui webjars
-                        "/swagger-resources/**",  // swagger-ui resources
-                        "/configuration/**",      // swagger configuration
+                        "/v2/api-docs",
+                        "/swagger-ui/**",
+                        "/webjars/**",
+                        "/swagger-resources/**",
+                        "/configuration/**",
                         "/*.html",
                         "/favicon.ico",
                         "/**/*.html",
